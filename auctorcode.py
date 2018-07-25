dee2bbd9f238f46463c5008fadbce611e1b285a4
hiciformatic <hicinformatic@gmail.com>
2018-07-25 16:53:50 +0200

    add auctor code

--- auctorcode.py
+++ auctorcode.py
@@ -1,35 +1,71 @@
 import re
 
+class AuctorTag(object):
+    pattern_open = re.compile(r"\[\{ac-?(\w+):?\(?([a-zA-Z0-9=\-\"# :;\.\/&]+)?\)?\}\]", re.MULTILINE)
+    pattern_close = re.compile(r"\[\{ac(\/?\w+)\}\]", re.MULTILINE)
+
+class AuctorAttribute(object):
+    test_case ="""
+    class="linux" style="color: #888; padding: 5px; font-weight: bold;" height="400"
+    class="linux" style="color: #888; padding: 5px; font-weight: bold;"
+    style="color: #888; padding: 5px; font-weight: bold;"
+    """
+    pattern = re.compile(r"[ |\(]([a-zA-Z0-9&]+)=\"([a-zA-Z0-9:\ #;\-]+)\"", re.MULTILINE)
+
 class AuctorCode(object):
-    content_ac = None
-    class tag(object):
-        name = None
-        tag = None
-        pattern = '\[([[:alpha:]]+)\](.*)\[/([[:alpha:]]+)]'
+    conf_tag = AuctorTag()
+    conf_attr = AuctorAttribute()
+    tags = {
+        'anchor':   { 'tag': 'a', },
+        'link':     { 'tag': 'a', },
+        'section':  { 'tag': 'section', 'attributes': ['style', 'height', 'width'], },
+        'article':  { 'tag': 'article', 'attributes': ['style', 'height', 'width'], },
+        'title1':   { 'tag': 'h1',      'attributes': ['style', 'height', 'width'], },
+        'title2':   { 'tag': 'h2',      'attributes': ['style', 'height', 'width'], },
+        'title3':   { 'tag': 'h3',      'attributes': ['style', 'height', 'width'], },
+        'title4':   { 'tag': 'h4',      'attributes': ['style', 'height', 'width'], },
+        'title5':   { 'tag': 'h5',      'attributes': ['style', 'height', 'width'], },
+        'title6':   { 'tag': 'h6',      'attributes': ['style', 'height', 'width'], },
+        'para':     { 'tag': 'p' ,      'attributes': ['style', 'height', 'width'],},
+        'list':     { 'tag': 'ul',      'attributes': ['style', 'height', 'width'], },
+        'listord':  { 'tag': 'ol',      'attributes': ['style', 'height', 'width'], },
+        'puce':     { 'tag': 'li',      'attributes': ['style', 'height', 'width'], },
+        'img':      { 'tag': 'img',     'attributes': ['style', 'height', 'width', 'src'], },
+        'code':     { 'tag': 'pre',     'attributes': ['style', 'height', 'width'], },
+        'terminal': { 'tag': 'pre',     'attributes': ['style', 'height', 'width'], },
+        'termcmd':  { 'tag': 'pre',     'attributes': ['style', 'height', 'width'], },
+        'termout':  { 'tag': 'pre',     'attributes': ['style', 'height', 'width'], },
+    }
+    attributes = {
+        'style': { 'attribute': 'style', 'pattern': re.compile(r"([a-zA-Z0-9:\ #;\-]+)") },
+        'link':  { 'attribute': 'src',   'pattern': re.compile(r"'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+") },
+    }
 
-    class attribute(object):
-        name = None
-        attribute = None
-        pattern = '\|%s(\(.+\))'
-        replace = ' %s="%s"'
+    pattern_open = r"\[\{ac-(%s)(:\()?(%s[\w\d\s[:punct:]]+)?\)?\}\]"
+    pattern_close = r"\[\{ac-(%s)\}\]"
+    tag_pattern_match = None
+    tag_pattern_replace = None
+    attribute_pattern_match = None
+    attribute_pattern_replace = None
+    content_input = None
+    content_output = None
 
-    class href(attribute):
-        name = 'href'
-        attribute = 'href'
+    def prepare(self):
+        self.tag_pattern_match = '|'.join(self.tags.keys())
+        self.tag_pattern_replace = '|'.join([tag['tag'] for key,tag in self.tags.items()])
+        self.attribute_pattern_match = '|'.join(self.attributes.keys())
+        self.attribute_pattern_replace = '|'.join([attribute['attribute'] for key,attribute in self.attributes.items()])
 
-    class link(tag):
-        name = 'link'
-        tag = 'a'
+    def to_html(self, text):
+            matches = self.conf_tag.pattern_open.findall(text)
+            #for match in matches:
+               # if match[0] not in self.tags:
+               #     print('no')
+            text = self.conf_tag.pattern_open.sub(r'<\1 \2>', text)
+            text = self.conf_tag.pattern_close.sub(r'<\1>', text)
+            #self.content = self.conf_tag.pattern_close.sub(r'<\1>', self.content)
+            return text
+        #return 'ko'
 
-    def __init__(self, content_ac):
-        self.content_ac = content_ac
-    
-    def Encoding(self):
-        return re.sub(self.tag.pattern, self.Replacing, self.content_ac)
 
-    def Replacing(self, matchobj):
-        from pprint import pprint
-        pprint(matchobj)
-        return 'test'
-        #return "<%s>" % getattr(getattr(self, matchobj.group(2)), tag)
         
--- tests.py
+++ tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
 
 # Create your tests here.
+class AuctorCodeTestCase(TestCase):
+    text_testcase = """
+    [{ac-section}]
+        [{ac-title:(style="color: #888;")}]Section title[{ac/title}]
+        [{ac-article}]
+            [{ac-title}]Article title[{ac/title}]
+            [{ac-para}]Lorem ipsum dolor sit amet, consectetur adipiscing elit.[{ac-smiley-laugh)}][{ac/para}]
+        [{ac/article}]
+
+        [{ac-article}]
+            [{ac-title}]Article title[{ac/title}]
+            [{ac-para:(style="color: #888" height="400")}]
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                [{ac-img:(src="https://url/img.jpg")}]
+                [{ac-list}]
+                    [{ac-puce}]puce 1[{ac/puce}]
+                    [{ac-puce}]puce 2[{ac/puce}]
+                [{ac/list}]
+            [{ac/para}]
+        [{ac/article}]
+
+        [{ac-code:(class="ac-php")}]
+            if ($test) {
+                echo "test";
+            }
+        [{ac/code}]
+
+        [{ac-terminal:(class="linux")}]
+            [{ac-termcmd:(class="linux")}]ps -ef[{ac/termcmd}]
+            [{ac-termout:(class="linux")}]root       2157      2  0 09:56 ?        00:00:26 [kworker/0:2][{ac/termout}]
+            [{ac-termout:(class="linux")}]root       3277      2  0 14:08 ?        00:00:00 [kworker/0:1][{ac/termout}]
+        [{ac/terminal}]
+    [{ac/section}]
+    """
+
+    def test_auctor_regex(self):
+        from pprint import pprint
+        from auctor.auctorcode import AuctorCode
+        ac = AuctorCode()
+        ac.prepare()
+        print('tags match: \n%s\n' % ac.tag_pattern_match)
+        print('tags replace: \n%s\n' % ac.tag_pattern_replace)
+        print('attributes match: \n%s\n' % ac.attribute_pattern_match)
+        print('attributes replace: \n%s\n' % ac.attribute_pattern_replace)
+
+    def test_auctor_tohtml(self):
+        from pprint import pprint
+        from auctor.auctorcode import AuctorCode
+        ac = AuctorCode()
+        ac.prepare()
+        #print(ac.to_html(self.text_testcase))
+
